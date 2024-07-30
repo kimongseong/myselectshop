7dc3c113a56605ed025e22108076e8633289c30a
hsk <ghdtjd1502@gmail.com>
2024-07-31 01:10:04 +0900

    스케줄 기능구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
@@ -58,5 +59,13 @@ public class ProductService {
 
 
         return responseDtoList;
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRepository.findById(id).orElseThrow(() -> new NullPointerException("해당 상품이 존재하지않습니다."));
+
+        product.updateByItemDto(itemDto);
+
     }
 }
