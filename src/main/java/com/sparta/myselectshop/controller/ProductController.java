859a72fd5a4c353ee5408eeb583571531eccbec2
hsk <ghdtjd1502@gmail.com>
2024-07-31 00:49:26 +0900

    관심상품 희망 최저가 업데이트 api

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -1,13 +1,12 @@
 package com.sparta.myselectshop.controller;
 
+import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.entity.Product;
 import com.sparta.myselectshop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,16 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
 
+
     private final ProductService productService;
 
 
     @PostMapping("/products")
     public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
-
-
         return productService.createProduct(requestDto);
 
     }
 
 
+    @PutMapping("/products/{id}")
+    private ProductResponseDto updateProduct(@PathVariable Long id , @RequestBody ProductMypriceRequestDto requestDto) {
+
+
+       return productService.updateProduct(id,requestDto);
+
+
+    }
 }
