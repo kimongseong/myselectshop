7dcc844cff66564cf73caba71e8ced7dba40d06d
hsk <ghdtjd1502@gmail.com>
2024-07-31 15:01:47 +0900

    폴더에 관심상품 추가 및 폴더 전체조회 구현

--- src/main/java/com/sparta/myselectshop/controller/ProductController.java
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -54,5 +54,14 @@ public class ProductController {
 
     }
 
+    @PostMapping("/products/{productId}/folder")
+    public void addFolder(@PathVariable Long productId ,
+                          @RequestParam Long folderId,
+                          @AuthenticationPrincipal UserDetailsImpl userDetails ) {
+
+        productService.addFolder(productId,folderId,userDetails.getUser());
+
+    }
+
 
 }
